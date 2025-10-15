c77ad206e110925b715d2fae5a4a5ee4a9c615b9
MaxChip <1max0lag1@gmail.com>
2025-10-15 09:29:55 -0400

    everything is almost done

--- src/main.go
+++ src/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,12 +17,41 @@ func GetContent(path string) (string, error) {
 	return string(data), nil
 }
 
-func Parse() {
-
-}
-
-func Interperet() {
-
+func Interperet(content string) error {
+	variables := make(map[string]byte)
+	loaded := []byte{0}
+	mentioned_variable := ""
+	for _, v := range content {
+		switch v {
+		case ',': // unload current memory
+			loaded[0] = 0
+		case '.': // load mentioned variable
+			loaded[0] = variables[mentioned_variable]
+		case '!': // write loaded memory
+			_, err := os.Stdout.Write(loaded)
+			if err != nil {
+				return err
+			}
+		case '+': // increment loaded memory
+			loaded[0]++
+		case '-': // decrement loaded memory
+			loaded[0]--
+		case ';': // forget mentioned variable
+			mentioned_variable = ""
+		case ':': // save mentioned variable
+			variables[mentioned_variable] = loaded[0]
+		case '?': // read to memory
+			_, err := os.Stdin.Read(loaded)
+			if err == io.EOF {
+				return nil
+			} else if err != nil {
+				return err
+			}
+		default:
+			mentioned_variable += string(v)
+		}
+	}
+	return nil
 }
 
 func main() {
@@ -40,11 +71,12 @@ func main() {
 		}
 	}
 
-	if len(args)-start <= 0 {
+	if len(args)-start <= 1 {
 		log.Fatal("no input file")
 	}
 	content, err := GetContent(args[start+1])
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println(content)
 }
